6abb4b3acd0f28671fcbbbcfad2550a2472d0db4
Dandan MENG <mengdandanno1@163.com>
2016-07-18 10:42:45 +0800

    set up the normal view pager demo with fragment, no looping effect yet

--- app/src/main/java/com/ddmeng/loopingfragmentpager/MainActivity.java
+++ app/src/main/java/com/ddmeng/loopingfragmentpager/MainActivity.java
@@ -1,13 +1,41 @@
 package com.ddmeng.loopingfragmentpager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int NUM_ITEMS = 10;
+
+    MyAdapter mAdapter;
+
+    ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdapter = new MyAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        // Watch for button clicks.
+        Button button = (Button) findViewById(R.id.goto_first);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mPager.setCurrentItem(0);
+            }
+        });
+        button = (Button) findViewById(R.id.goto_last);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mPager.setCurrentItem(NUM_ITEMS - 1);
+            }
+        });
     }
 }
